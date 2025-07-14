8d07cecc911ec1123087d166cb195d2c1256a913
Oppong Coffie <108099961+oppong-coffie@users.noreply.github.com>
2025-07-14 11:49:26 +0000

    Update servedModel.php

--- app/Models/servedModel.php
+++ app/Models/servedModel.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class servedModel extends Model
+class ServedModel extends Model
 {
     use HasFactory;
     protected $table ='served'; //table name
